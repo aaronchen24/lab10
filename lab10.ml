ac2e647e5cdf969b3cd8efeb5e7b9df41b95a135
aaronchen24 <misteraaronchen@gmail.com>
2019-03-10 16:27:31 -0400

    Update lab10.ml

--- lab10.ml
+++ lab10.ml
@@ -29,7 +29,7 @@ specified length of random integers between 0 and 999.
 ....................................................................*)
 
 let random_list (length : int) : int list =
-  failwith "random_list not implemented" ;;
+  List.init length (fun _ -> Random.int 1000) ;;
 
 (*....................................................................
 Exercise 2: Write a function, time_sort, that, given an int list ->
@@ -38,7 +38,8 @@ sort takes.
 ....................................................................*)
 
 let time_sort (sort : int list -> int list) (lst : int list) : float =
-  failwith "time_sort not implemented";;
+  let _, time = CS51.call_timed sort lst in
+  time;;
 
 (* We've provided implementations of merge sort and insertion sort
 here as modules satisfying the SORT signature so that you have some
@@ -107,7 +108,8 @@ module MergeSort : SORT =
 Exercise 3: List the functions provided by the InsertionSort
 module. List the functions provided by the MergeSort module.
 ....................................................................*)
-
+    (* Both modules provide a sort function, and the insert, split, and merge
+    functions are inaccessible outside the module. *)
   
 (*....................................................................
 Exercise 4: Compare the time it takes for merge sort and insertion
@@ -122,9 +124,9 @@ Fill in the table below:
                 |    List length 10    |  List length 1000
                 |    Time (seconds)    |  Time (seconds)
 ------------------------------------------------------------
-Insertion Sort  |                      |
-------------------------------------------------------------
-Merge Sort      |                      |
+Insertion Sort  |    0.00000229        |   0.0111
+    ------------------------------------------------------------
+Merge Sort      |    0.00000498        |   0.000921
 ------------------------------------------------------------
 
 ......................................................................*)
@@ -177,19 +179,19 @@ let exercise5a () : complexity list =
 
 (* f(x) = 0 *)
 let exercise5b () : complexity list =
-  failwith "exercise5b not implemented" ;;
+  [Constant; Logarithmic; Linear; LogLinear; Quadratic; Cubic; Exponential] ;;
 
 (* f(x) = 3 x^2 + 2 x + 4 *)
 let exercise5c () : complexity list=
-  failwith "exercise5c not implemented" ;;
+  [Quadratic; Cubic; Exponential] ;;
 
 (* f(x) = (2 x - 3) log(x) + 100 x *)
 let exercise5d () : complexity list =
-  failwith "exercise5d not implemented" ;;
+  [LogLinear; Quadratic; Cubic; Exponential] ;;
 
 (* f(x) = x (x^2 + x) *)
 let exercise5e () : complexity list =
-  failwith "exercise5e not implemented" ;;
+  [Cubic; Exponential] ;;
 
 
 (* One advantage of big-O is that we can disregard constants in
@@ -227,9 +229,9 @@ Fill in the below table.
 (*               |    List length 10    |  List length 1000
                  |    Time (seconds)    |  Time (seconds)
 ------------------------------------------------------------
-Insertion Sort   |                      |
+Insertion Sort   |    0.00000241        |  0.0119
 ------------------------------------------------------------
-Delay Merge Sort |                      |
+Delay Merge Sort |    0.0529            |  0.0563                  
 ------------------------------------------------------------*)
 
 (* You likely found that InsertSort was faster than DelayMergeSort,
@@ -238,7 +240,7 @@ sorted by DelayMergeSort and InsertSort until DelayMergeSort runs
 faster than InsertSort. Record the size of a list for which this is
 true below. *)
    
-let exercise6 () = failwith "exercise6 not implemented"
+let exercise6 () = 10000 ;;
 
 (* Big-O also allows us to disregard constant multiplicative factors. In
 this exercise, we work with a version of MergeSort that sorts a given
@@ -267,15 +269,15 @@ table below.
 (*                |    List length 10    |  List length 1000
                   |    Time (seconds)    |  Time (seconds)
 ------------------------------------------------------------
-Insertion Sort    |                      |
+Insertion Sort    |    0.00000136        |  0.00965
 ------------------------------------------------------------
-Double Merge Sort |                      |
+Double Merge Sort |    0.00000926        |  0.00169                 
 ------------------------------------------------------------*)
 
 (* Now record a list length for which you found DoubleMergeSort
 sorted faster than InsertSort. *)
    
-let exercise7 () = failwith "exercise6 not implemented"
+let exercise7 () = 1000 ;;
 
 (* An additional nice property of big-O is the ability to disregard
 lower-order terms of a function. In the reading, we found that:
@@ -308,14 +310,14 @@ below.
 (*                    |    List length 10    |  List length 1000
                       |    Time (seconds)    |  Time (seconds)
 -----------------------------------------------------------------
-Insertion Sort        |                      |
+Insertion Sort        |   0.00000249         |  0.0112
 -----------------------------------------------------------------
-Extra Term Merge Sort |                      |
+Extra Term Merge Sort |   0.00000628         |  0.00119               
 -----------------------------------------------------------------*)
 
 (* Now record a list length for which ExtraTermMergeSort works faster
     than InsertSort. *)
-let exercise8 () = failwith "exercise6 not implemented"
+let exercise8 () = 1000;;
 
 (*......................................................................
 Exercise 9: More big-O
@@ -336,24 +338,23 @@ type complexity =
 
 (* f(x) = 10000 *)
 let exercise9a () : complexity list =
-  failwith "exercise9a not implemented" ;;
-  
+  [Constant; Logarithmic; Linear; LogLinear; Quadratic; Cubic; Exponential] ;;
+
 (* f(x) = 50x^100 + x^2 *)
 let exercise9b () : complexity list =
-  failwith "exercise9b not implemented" ;;
+  [Exponential] ;;
 
 (* f(x) = 30xlog(x) + 50x + 70 *)
-
 let exercise9c () : complexity list =
-  failwith "exercise9c not implemented" ;;
+  [LogLinear; Quadratic; Cubic; Exponential] ;;
 
 (* f(x) = 30x^2 * log(x) *)
 let exercise9d () : complexity list =
-  failwith "exercise9d not implemented" ;;
+  [Cubic; Exponential] ;;
 
 (* f(x) = x + 60log(x) *)
 let exercise9e () : complexity list =
-  failwith "exercise9e not implemented" ;;
+    [Linear; LogLinear; Quadratic; Cubic; Exponential] ;;
 
 (*======================================================================
 Part 3: Recurrence Equations
@@ -410,11 +411,12 @@ let rec insert xs x =
 Complete the recurrence equations and time complexity of this function:
 ......................................................................*)
 
-let time_insert (n : int) : int =
-    failwith "time_insert not yet implemented" ;;
+let rec time_insert (n : int) : int =
+    if n = 0 then k
+    else k + time_insert (n - 1) ;;
 
 let insert_complexity () : complexity =
-    failwith "insert_complexity not yet implemented" ;;
+    Linear ;;
 (*......................................................................
                             SOLUTION
 
@@ -468,12 +470,13 @@ let rec sum (x : int list) : int =
 
 (* Describe the time complexity recurrence equations for sum as an
 ocaml function *)
-let time_sum (n : int) : int =
-  failwith "time_sum not yet implemented" ;;
+let rec time_sum (n : int) : int =
+  if n = 0 then k
+  else k + time_sum (n - 1);;
 
 (* What is its complexity? *) 
 let sum_complexity () : complexity =
-  failwith "sum_complexity not yet implemented" ;;
+  Linear;;
 
 (*......................................................................
 Exercise 11: Divider Recurrence Equations
@@ -487,11 +490,12 @@ let rec divider (x : int) : int =
   else if x <= 1 then 0
   else 1 + divider (x / 2);;
   
-let time_divider (n : int) : int =
-  failwith "time_sum not yet implemented" ;;
+let rec time_divider (n : int) : int =
+  if n = 0 || n = 1 then k
+  else k + time_divider (n / 2);;
 
 let divider_complexity () : complexity =
-  failwith "time_complexity not yet implemented" ;;
+  Logarithmic;;
 
 (*......................................................................
 Exercise 12: Find_min recurrence equations
@@ -517,17 +521,19 @@ let rec find_min (xs : int list) : int =
   | _ -> let l1, l2 = split xs in
          min (find_min l1) (find_min l2) ;;
 
-let time_split (n : int) : int =
-  failwith "time_split not yet implemented" ;;
+let rec time_split (n : int) : int =
+  if n = 0 || n = 1 then k
+  else k + time_split (n - 2) ;;
 
 let split_complexity () : complexity =
-  failwith "split_complexity not yet implemented" ;;
+  Linear ;;
 
-let time_find_min (n : int) : int =
-  failwith "time_find_min not yet implemented" ;;
+let rec time_find_min (n : int) : int =
+  if n = 0 || n = 1 then k
+  else (time_split n) + 2 * time_find_min (n / 2) ;;
 
 let find_min_complexity () : complexity =
-  failwith "find_min_complexity not yet implemented" ;;
+    LogLinear ;;
 
 
 (*======================================================================
@@ -618,7 +624,8 @@ let time_multiply (mult : int -> int -> int)
                   (x : int)
                   (y : int)
                 : float =
-  failwith "time_multiply not yet implemented";;
+  let _, time = CS51.call_timed (fun (x, y) -> mult x y) (x, y) in
+  time ;;
 
 (*......................................................................
 Exercise 14: Fill in the table below:
@@ -628,13 +635,13 @@ Exercise 14: Fill in the table below:
                        |                      |  3237461243
                        |    Time (seconds)    |  Time (seconds)
 -----------------------------------------------------------------
-Repeated Addition      |                      |
+Repeated Addition      |     0.00000277       |  83.6
 -----------------------------------------------------------------
-Grade School Algorithm |                      |
+Grade School Algorithm |     0.00000351       |  0.0000135
 -----------------------------------------------------------------
-Karatsuba              |                      |
+Karatsuba              |     0.00000747       |  0.0000261
 -----------------------------------------------------------------
-OCaml Native ( * )     |                      |
+OCaml Native ( * )     |     0.00000115       |  ~0
 -----------------------------------------------------------------
 ....................................................................*)
 (*......................................................................
